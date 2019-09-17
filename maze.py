3b293ee86d3c5c14db24dcd4acf06ee479a47331
Chaitanya <nalamchaitanya@gmail.com>
2019-09-16 20:05:50 -0400

    Changed maze implementation to matrix

--- maze.py
+++ maze.py
@@ -6,14 +6,16 @@ class Maze:
 	mazeCells = {};
 
 def generateMaze(dim, p):
-	mazeCells = defaultdict(list);
+	mazeCells = [];
 	for i in range(dim):
+		mazeCells.append([]);
 		for j in range(dim):
-			if(random.uniform(0,1) < p):
-				mazeCells[i].append(j);
+			if(random.uniform(0,1) <= p):
+				mazeCells[i].append(1);
+			else:
+				mazeCells[i].append(0);
 	return mazeCells;
 
 mazeCells = generateMaze(10,0.6);
 for i in range(10):
-	print (str(i)  + mazeCells[i])
-# print(mazeCells)
+	print (str(i) + ":"  + str(mazeCells[i]));
