9b632851e7f5061dd41d680fc1eabd429049fab2
Chaitanya <nalamchaitanya@gmail.com>
2019-09-17 20:34:41 -0400

    Added Maze class

--- maze.py
+++ maze.py
@@ -1,30 +1,37 @@
 import random;
 
 class Maze:
-	# bool flag;
-	mazeCells =[]
+	# int dimension;
+	# float probability;
+	# list mazeCells;
 
-	def generateMaze(dim, p):
-		for i in range(dim):
-			for j in range(dim):
-				if (random.uniform(0, 1) <= p):
-					mazeCells[i][j] = 1;
+	def __init__(self, dim, p):
+		self.dimension = dim;
+		self.probability = p;
+		self.mazeCells = [];
+		for i in range(self.dimension):
+			self.mazeCells.append([]);
+			for j in range(self.dimension):
+				if (random.uniform(0, 1) <= self.probability):
+					self.mazeCells[i].append(1);
 				else:
-					mazeCells[i][j] = 0;
-		return mazeCells;
+					self.mazeCells[i].append(0);
+		self.mazeCells[0][0] = 0;
+		self.mazeCells[dimension-1][dimension-1] = 0;
 
-	def giveEligibleChild(x, y, dim):
-		tuple=[]
+	def giveEligibleChild(self, x, y):
+		children=[]
 		if x-1 >= 0:
-			if mazeCells[x-1][y]==1:
-				tuple.append((x-1,y))
-		if x+1<=dim-1:
-			if mazeCells[x+1][y]==1:
-				tuple.append((x+1,y))
+			if self.mazeCells[x-1][y]==1:
+				children.append((x-1,y))
+		if x+1<=self.dimension-1:
+			if self.mazeCells[x+1][y]==1:
+				children.append((x+1,y))
 		if y-1>=0:
-			if mazeCells[x][y-1]==1:
-				tuple.append((x,y-1))
-		if y+1<=dim-1:
-			if mazeCells[x][y+1]==1:
-				tuple.append((x,y+1))
-		return tuple
+			if self.mazeCells[x][y-1]==1:
+				children.append((x,y-1))
+		if y+1<=self.dimension-1:
+			if self.mazeCells[x][y+1]==1:
+				children.append((x,y+1))
+		return children;
+
