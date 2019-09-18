79a59e65bd324990a0d9353a3e667993ee3f7e58
Chaitanya <nalamchaitanya@gmail.com>
2019-09-17 23:05:49 -0400

    Added tree search

--- maze.py
+++ maze.py
@@ -17,21 +17,34 @@ class Maze:
 				else:
 					self.mazeCells[i].append(0);
 		self.mazeCells[0][0] = 0;
-		self.mazeCells[dimension-1][dimension-1] = 0;
+		self.mazeCells[self.dimension-1][self.dimension-1] = 0;
 
 	def giveEligibleChild(self, x, y):
 		children=[]
 		if x-1 >= 0:
-			if self.mazeCells[x-1][y]==1:
+			if self.mazeCells[x-1][y]==0:
 				children.append((x-1,y))
 		if x+1<=self.dimension-1:
-			if self.mazeCells[x+1][y]==1:
+			if self.mazeCells[x+1][y]==0:
 				children.append((x+1,y))
 		if y-1>=0:
-			if self.mazeCells[x][y-1]==1:
+			if self.mazeCells[x][y-1]==0:
 				children.append((x,y-1))
 		if y+1<=self.dimension-1:
-			if self.mazeCells[x][y+1]==1:
+			if self.mazeCells[x][y+1]==0:
 				children.append((x,y+1))
 		return children;
 
+	def treeSearch(self, (startx, starty), (endx , endy)):
+		fringe = [(startx,starty, [(startx, starty)])];
+		closedSet = [];
+		while (len(fringe) != 0):
+			(x, y, path) = fringe.pop();
+			if (x,y) not in closedSet:
+				if (x,y) == (endx,endy):
+					return path;
+				eligibleChildren = self.giveEligibleChild(x,y);
+				for (cx,cy) in eligibleChildren:
+					fringe.append((cx,cy,path + [(cx,cy)]));
+				closedSet.append((x,y));
+		return [];
