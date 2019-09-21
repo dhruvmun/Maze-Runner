70e5c7426bf393bbf799722742bfd86a3125fa56
Chaitanya <nalamchaitanya@gmail.com>
2019-09-20 23:32:39 -0400

    Added visualization to question 2.2

--- question22.py
+++ question22.py
@@ -1,8 +1,9 @@
 import maze
 import helper
 import time
+import visualization
 
-dimension = 200
+dimension = 15
 probability = 0.2
 path = []
 while len(path) == 0:
@@ -12,22 +13,36 @@ print "Dimension : " + str(dimension) + " Probability : " + str(probability)
 
 startTime = time.time()
 dfsPath = mazeObject.dfs()
-dfsPath2 = mazeObject.treeSearch()
-print dfsPath == dfsPath2
 endTime = time.time()
 print "DFS time: " + str(endTime-startTime) + "sec Length : " + str(len(dfsPath))
+
+visualise = visualization.Visualize("DFS", 'yellow')
+visualise.drawMaze(mazeObject)
+visualise.drawPath(dfsPath)
 
 startTime = time.time()
 bfsPath = mazeObject.BFS()
 endTime = time.time()
 print "BFS time: " + str(endTime-startTime) + "sec Length : " + str(len(bfsPath))
-		
+
+visualise = visualization.Visualize("BFS", 'green')
+# visualise.drawMaze(mazeObject)
+visualise.drawPath(bfsPath)
+
 startTime = time.time()
 euclidPath = mazeObject.aStarSearch(helper.euclidDistance)
 endTime = time.time()
 print "Euclid A* time: " + str(endTime-startTime) + "sec Length : " + str(len(euclidPath))
 
+visualise = visualization.Visualize("Euclid A*", 'red')
+# visualise.drawMaze(mazeObject)
+visualise.drawPath(euclidPath)
+
 startTime = time.time()
 manhattanPath = mazeObject.aStarSearch(helper.manhattanDistance)
 endTime = time.time()
 print "Manhattan A* time: " + str(endTime-startTime) + "sec Length : " + str(len(manhattanPath))
+
+visualise = visualization.Visualize("Manhattan A*", 'blue')
+# visualise.drawMaze(mazeObject)
+visualise.drawPath(manhattanPath)
